a5c0471eb9d2d2783ae968f8ef100a781c1fcc19
xceptions <agbokenechukwu.k@gmail.com>
2023-08-01 15:47:55 +0000

    made few changes to the html

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&23sam!l24l@g6ri627r8xma(&gqf*f^7)o$zvk+e@q&u0*3h!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '34.171.199.103']
 # CORS_ORIGIN_WHITELIST = [
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
